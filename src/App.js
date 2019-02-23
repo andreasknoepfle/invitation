bd4497dc6b5669f18121945e502c86d0432518e1
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2019-02-23 01:56:59 +0100

    first draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react';
+import queryString from 'query-string'
 import logo from './logo.svg';
 import './App.css';
+import FoodRadio from './FoodRadio';
+import 'whatwg-fetch'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaded: false };
+  }
+
+  initFood() {
+    fetch(`https://wt-9933bad0e1d0fa1a4419e30b982036ad-0.sandbox.auth0-extend.com/airtable-get?key=${this.key()}`)
+      .then(response => response.json())
+      .then(({ food }) => this.setState({ food, loaded: true }) )
+  }
+
+  onChangeFood({ target }) {
+    this.setState({ food: target.value }, () => this.submit());
+  }
+
+  submit() {
+    fetch(`https://wt-9933bad0e1d0fa1a4419e30b982036ad-0.sandbox.auth0-extend.com/airtable-hook?key=${this.key()}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ food: this.state.food })
+    });
+  }
+
+  componentDidMount() {
+    this.initFood();
+  }
+
+  key() {
+    return queryString.parse(window.location.search).key;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <form>
+            Was möchtest du essen?
+            <br />
+            <FoodRadio
+              label="Vegetarisch"
+              disabled={!this.state.loaded}
+              checked={this.state.food === "Vegetarisch"}
+              onChange={this.onChangeFood.bind(this)}
+            />
+            <FoodRadio
+              label="Vegan"
+              disabled={!this.state.loaded}
+              checked={this.state.food === "Vegan"}
+              onChange={this.onChangeFood.bind(this)}
+            />
+            <FoodRadio
+              label="Alles"
+              disabled={!this.state.loaded}
+              checked={this.state.food === "Alles"}
+              onChange={this.onChangeFood.bind(this)}
+            />
+          </form>
         </header>
       </div>
     );
---
+++ src/FoodRadio.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faCircle } from '@fortawesome/free-solid-svg-icons';
+const FoodRadio = ({ checked, label, ...props }) => (
+  <label class='food-radio'>
+    {
+      checked ? <FontAwesomeIcon icon={faCheckCircle} /> : <FontAwesomeIcon icon={faCircle} />
+    }
+    <input
+      type="radio"
+      name="food"
+      value={label}
+      checked={checked}
+      style={{ display: 'none'}}
+      {...props}
+    /> {label}
+  </label>
+);
+
+export default FoodRadio;
